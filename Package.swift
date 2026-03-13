5673ba0b35d91824182fac10a81717c3b513ded5
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-03-13 10:04:32 +0100

    Save progress: 2026-03-13

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
         .library(name: "PDF Standard", targets: ["PDF Standard"])
     ],
     dependencies: [
-        .package(path: "../swift-iso-32000"),
+        .package(path: "../../swift-iso/swift-iso-32000"),
         // .package(url: "https://github.com/pointfreeco/swift-snapshot-testing", from: "1.18.0")
     ],
     targets: [
